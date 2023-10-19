c431ede1278579251da20b507c56e486982553b2
Tony B <142834478+TonyBaas@users.noreply.github.com>
2023-10-19 13:11:19 -0500

    Initial commit

---
+++ InnerZenApp/InnerZenApp Watch App/InnerZenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnerZenAppApp.swift
+//  InnerZenApp Watch App
+//
+//  Created by Tony Baas on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct InnerZenApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
