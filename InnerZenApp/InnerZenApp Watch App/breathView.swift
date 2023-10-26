aed4be43d6a85c9550b8391921a0043683c02cee
TonyBaas <142834478+TonyBaas@users.noreply.github.com>
2023-10-26 09:45:32 -0500

    Changes: - Added BreathView - Added mindfulView

---
+++ InnerZenApp/InnerZenApp Watch App/breathView.swift
@@ -0,0 +1,18 @@
+//
+//  breathView.swift
+//  InnerZenApp Watch App
+//
+//  Created by Tony Baas on 10/26/23.
+//
+
+import SwiftUI
+
+struct breathView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    breathView()
+}
---
+++ InnerZenApp/InnerZenApp Watch App/mindfulView.swift
@@ -0,0 +1,18 @@
+//
+//  mindfulView.swift
+//  InnerZenApp Watch App
+//
+//  Created by Tony Baas on 10/26/23.
+//
+
+import SwiftUI
+
+struct mindfulView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mindfulView()
+}
