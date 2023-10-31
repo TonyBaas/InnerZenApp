09e89534c59728663c9f83a3b6be4577d894de78
Reaenn Wiebers <Reaenn.wiebers@southeasttech.edu>
2023-10-31 10:57:10 -0500

    changes:     added labels and timer

--- InnerZenApp/InnerZenApp Watch App/mindfulView.swift
+++ InnerZenApp/InnerZenApp Watch App/mindfulView.swift
@@ -9,8 +9,19 @@
 import SwiftUI
 
 struct mindfulView: View {
+    @State var timeRemaining = 60
+        let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+                    
     var body: some View {
         Label("Mood", systemImage: "theatermasks")
+        Label("Word", systemImage: "brain")
+        Label("", systemImage: "")
+        Text("\(timeRemaining)")
+            .onReceive(timer) { _ in
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                }
+            }
     }
 }
 
