245aaa9a043ed9c55402205f8a895e5bd969bfbd
TonyBaas <142834478+TonyBaas@users.noreply.github.com>
2023-11-02 10:02:12 -0500

    Changes: - Added Random Qoute - Added NavigationLinks to other views - Added NavigationLink that choose a random qoute once pressed.

--- InnerZenApp/InnerZenApp Watch App/ContentView.swift
+++ InnerZenApp/InnerZenApp Watch App/ContentView.swift
@@ -9,17 +9,58 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var quotes = ["Everything is Fine.", "Keep it off campus guys.", "Read the writing on the wall."]
+    @State private var isShuffling = true
+    
+    @State private var currentQuote = "Random Quote"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("InnerZen")
+        
+        NavigationView {
+            VStack{
+                HStack{
+                    Text("InnerZen")
+                    Text("                Logo")
+                }
+                NavigationLink(destination: EmptyView()) {
+                    Text(currentQuote)
+                        .onTapGesture {
+                            updateCurrentQuote()
+                        }
+                        .font(.system(size: 13))
+                }
+                NavigationLink(destination: mindfulView()) {
+                    Text("")
+                }
+                 // Adjust the corner radius as needed
+                NavigationLink(destination: breathView()) {
+                    
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            updateCurrentQuote() // Initial quote
+            startQuoteTimer()
+        }
+    }
+    
+    func updateCurrentQuote() {
+        let randomIndex = Int.random(in: 0..<quotes.count)
+        currentQuote = quotes[randomIndex]
+    }
+
+    func startQuoteTimer() {
+        Timer.scheduledTimer(withTimeInterval: 10, repeats: true) { _ in
+            updateCurrentQuote() // Update the quote every 10 seconds
+        }
     }
 }
+
+
+
 
 #Preview {
     ContentView()
