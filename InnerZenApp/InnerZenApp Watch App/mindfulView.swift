cc34dd2fc90a199a6faf9d3e931b5bc748800b75
Reaenn Wiebers <Reaenn.wiebers@southeasttech.edu>
2023-11-05 04:37:42 -0600

    changes:     Got my mood field working

--- InnerZenApp/InnerZenApp Watch App/mindfulView.swift
+++ InnerZenApp/InnerZenApp Watch App/mindfulView.swift
@@ -14,11 +14,16 @@ struct mindfulView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @State private var words = ["Peace", "Acceptance", "Confidence","Zen","Dead Bird","Gratitude"]
     
-    @State private var currentWord = "Random Word"
+    @State private var currentWord = "Focus Word"
+    
+    @FocusState private var isFocused: Bool
+    @State private var text = "What is your mood?"
+    
     
     func start(){
         timeRemaining = 60
     }
+    
     //updating words for random focus word
     func updateCurrentWord() {
         let randomIndex = Int.random(in: 0..<words.count)
@@ -26,21 +31,33 @@ struct mindfulView: View {
     }
                 
     var body: some View {
+        
         Label("Mood", systemImage: "theatermasks")
-        Label("Word", systemImage: "brain");                    Text(currentWord)
-            .onTapGesture {
-                updateCurrentWord()
-            }
-        Label("", systemImage: "")
-        //timer for mindful moment
-        Button(action: start){
-            Text("\(timeRemaining)")
-                .onReceive(timer) { _ in
-                    if timeRemaining > 0 {
-                        timeRemaining -= 1
-                    }
-                }
+        HStack{
+            
+            TextField(text: $text) {
+                Text("Tap, touch and hold, or swipe left to rename")
+            }.focused($isFocused)
+            
         }
+        HStack{
+            Label("Word:", systemImage: "brain");                    Text(currentWord)
+                .onTapGesture {
+                    updateCurrentWord()
+                }
+            Label("", systemImage: "")
+        }
+            
+            //timer for mindful moment
+            Button(action: start){
+                Text("\(timeRemaining)")
+                    .onReceive(timer) { _ in
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1
+                        }
+                    }
+            }
+        
     }
 }
 
